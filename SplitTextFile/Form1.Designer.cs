61f3f6b988d2ca4f5673c96115dfe0ed0914f226
Đỗ Đăng Thuỷ <dodangthuy83@yahoo.com>
2021-11-18 23:30:23 +0700

    Add project files.

---
+++ SplitTextFile/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace SplitTextFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtSoLuong = new System.Windows.Forms.TextBox();
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.BtnStop = new System.Windows.Forms.Button();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.BtnBrowser = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TxtPath
+            // 
+            this.TxtPath.Enabled = false;
+            this.TxtPath.Location = new System.Drawing.Point(74, 34);
+            this.TxtPath.Name = "TxtPath";
+            this.TxtPath.Size = new System.Drawing.Size(284, 27);
+            this.TxtPath.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(4, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Path:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(4, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Số Lượng/File";
+            // 
+            // TxtSoLuong
+            // 
+            this.TxtSoLuong.Location = new System.Drawing.Point(184, 80);
+            this.TxtSoLuong.Name = "TxtSoLuong";
+            this.TxtSoLuong.PlaceholderText = "Nhập Số Lượng";
+            this.TxtSoLuong.Size = new System.Drawing.Size(174, 27);
+            this.TxtSoLuong.TabIndex = 3;
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Location = new System.Drawing.Point(49, 130);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(94, 29);
+            this.BtnStart.TabIndex = 4;
+            this.BtnStart.Text = "Start";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // BtnStop
+            // 
+            this.BtnStop.Location = new System.Drawing.Point(166, 130);
+            this.BtnStop.Name = "BtnStop";
+            this.BtnStop.Size = new System.Drawing.Size(94, 29);
+            this.BtnStop.TabIndex = 5;
+            this.BtnStop.Text = "Stop";
+            this.BtnStop.UseVisualStyleBackColor = true;
+            this.BtnStop.Click += new System.EventHandler(this.BtnStop_Click);
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.Location = new System.Drawing.Point(283, 130);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(94, 29);
+            this.BtnExit.TabIndex = 6;
+            this.BtnExit.Text = "Exit";
+            this.BtnExit.UseVisualStyleBackColor = true;
+            this.BtnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // BtnBrowser
+            // 
+            this.BtnBrowser.Location = new System.Drawing.Point(358, 33);
+            this.BtnBrowser.Name = "BtnBrowser";
+            this.BtnBrowser.Size = new System.Drawing.Size(65, 28);
+            this.BtnBrowser.TabIndex = 2;
+            this.BtnBrowser.Text = ".........";
+            this.BtnBrowser.UseVisualStyleBackColor = true;
+            this.BtnBrowser.Click += new System.EventHandler(this.BtnBrowser_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 183);
+            this.Controls.Add(this.BtnBrowser);
+            this.Controls.Add(this.BtnExit);
+            this.Controls.Add(this.BtnStop);
+            this.Controls.Add(this.BtnStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtSoLuong);
+            this.Controls.Add(this.TxtPath);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Split Text File";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox TxtPath;
+        private Label label1;
+        private Label label2;
+        private TextBox TxtSoLuong;
+        private Button BtnStart;
+        private Button BtnStop;
+        private Button BtnExit;
+        private Button BtnBrowser;
+    }
+}
---
+++ SplitTextFile/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System.ComponentModel;
+using System.IO;
+
+namespace SplitTextFile
+{
+    public partial class Form1 : Form
+    {
+        BackgroundWorker _backgroundWorker = new BackgroundWorker();        
+        public Form1()
+        {
+            InitializeComponent();
+            _backgroundWorker.DoWork += BackgroundWorker_DoWork;
+            _backgroundWorker.RunWorkerCompleted += BackgroundWorker_RunWorkerCompleted;
+        }
+
+        private void BackgroundWorker_RunWorkerCompleted(object? sender, RunWorkerCompletedEventArgs e)
+        {
+            _ = MessageBox.Show("TẠO FILE THÀNH CÔNG");
+        }
+
+        private void BackgroundWorker_DoWork(object? sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                int l_counter = 0;
+                int spl_counter = 1;
+                string line;
+                string saveFile = Application.StartupPath + @"/File Split";
+                if (!Directory.Exists(saveFile))
+                {
+                    _ = Directory.CreateDirectory(saveFile);
+                }
+                StreamWriter? swWriter = new(saveFile + @"/ouput.txt");
+                StreamReader swReader = new(TxtPath.Text);
+                while ((line = swReader.ReadLine()) != null)
+                {
+                    swWriter.WriteLine(line);
+                    l_counter++;
+
+                    if (l_counter >= int.Parse(TxtSoLuong.Text))
+                    {
+                        swWriter.Close();
+                        swWriter = null;
+                        swWriter = new StreamWriter(saveFile + @"/output_" + spl_counter + ".txt");
+                        spl_counter++;
+                        l_counter = 0;
+                    }
+                }
+
+                // Close the stream reader / writer
+                swReader.Close();
+                swWriter.Close();
+
+            }
+            catch (Exception ex)
+            {
+                _ = MessageBox.Show("KHÔNG TẠO ĐƯỢC FILE" + Environment.NewLine + ex);
+            }
+        }
+
+        private void BtnBrowser_Click(object sender, EventArgs e)
+        {
+            Openfile();
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            if(!_backgroundWorker.IsBusy)
+            {
+                _backgroundWorker.RunWorkerAsync();
+            }
+        }
+
+        private void BtnStop_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private void Openfile()
+        {
+            using OpenFileDialog openFileDialog = new() { Filter = "Text File(*.txt)|*.txt|All File(*.*)|*.*", Multiselect = false };
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                TxtPath.Text = openFileDialog.FileName;
+            }
+        }
+    }
+}
